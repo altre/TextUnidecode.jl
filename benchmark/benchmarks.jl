c188345306902ec989766f482e855d70bc20ff78
Alan Schelten <alan.schelten@mercateo.com>
2019-10-20 21:23:34 +0200

    Small updates.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,4 +1,4 @@
 using TextUnidecode, BenchmarkTools
-using BenchmarkTools
 const SUITE = BenchmarkGroup()
 SUITE["南无阿弥陀佛"] = @benchmarkable unidecode("南无阿弥陀佛")
+SUITE["really only ascii all fine!"] = @benchmarkable unidecode("really only ascii all fine!")
